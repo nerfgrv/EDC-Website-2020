3be3d64caeed79f48c46d3da6e5ee8a4c4011a0e
Chetan Bansal <chetanbansal1611@gmail.com>
2020-06-27 19:49:03 +0530

    manikblog

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import PostDetailView
+from .models import Post
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('post/<int:pk>/',PostDetailView.as_view(), name='blog-detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'post': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+def home1(request):
+    return render(request, 'blog/home1.html')
