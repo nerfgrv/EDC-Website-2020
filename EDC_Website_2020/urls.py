aa86db271587a4fafec531360f3c209f9695c578
Kajal-11 <kajal11122000@gmail.com>
2020-06-29 17:29:27 +0530

    user app

--- EDC_Website_2020/urls.py
+++ EDC_Website_2020/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
     path('', include('home.urls')),
+    path('',include('user.urls')),
+    path('blog/', include('blog.urls')),
     path('events/',include('event.urls')),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/signup.html', {'form': form})
+
+def profile(request):
+    return render(request, 'user/profile.html')
+
+
+def logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
