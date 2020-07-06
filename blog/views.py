e25e4a2bcc74cca372beac4616c16a5e8d9b4a00
Kajal-11 <kajal11122000@gmail.com>
2020-07-06 16:06:03 +0530

    Blog App UI Fix

--- blog/views.py
+++ blog/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from django.views.generic import DetailView, CreateView , UpdateView ,DeleteView
 from django.urls import reverse
 from .models import Post
-
+# from django.contrib.auth.decorators import user_passes_test
 
 def home(request):
     context = {
-        'post': Post.objects.all()
+        'posts': Post.objects.order_by('-date_posted')
     }
     return render(request, 'blog/home.html', context)
 
 class PostDetailView(DetailView):
     model = Post
 
+# @user_passes_test(lambda u: u.is_superuser)
 class PostCreateView(CreateView):
     model = Post
     fields=['title','content','author','image']
@@ -20,6 +21,7 @@ class PostCreateView(CreateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={'pk': self.object.id})
 
+# @user_passes_test(lambda u: u.is_superuser)
 class PostUpdateView(UpdateView):
     model = Post
     fields=['title','content','author','image']
@@ -27,7 +29,9 @@ class PostUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={'pk': self.object.id})
 
+# @user_passes_test(lambda u: u.is_superuser)
 class PostDeleteView(DeleteView):
     model = Post
-    success_url='/'
 
+    def get_success_url(self):
+        return reverse('blog-home')
