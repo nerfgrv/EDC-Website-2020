e99a3d2e24b7c0e16a05ce82f7c862bad48b3b64
N15H4N7 <ngupta_be19@thapar.edu>
2020-07-07 19:24:20 +0530

    Team Page

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',			views.home,		name='home'),
     path('our-team/',	views.team,	name='our-team'),
     path('our-team1/',	views.team1,	name='our-team'),
+    path('our-team2/',	views.team2,	name='our-team'),
 ]
