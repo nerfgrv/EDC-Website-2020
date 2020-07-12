dc91193262096c79f200c8d9a4ae304a34848223
N15H4N7 <ngupta_be19@thapar.edu>
2020-07-12 13:27:33 +0530

    event

--- internshipPortal/urls.py
+++ internshipPortal/urls.py
@@ -3,18 +3,7 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('internships/', 						views.Internships, name='internships'),
-    path('internships/create/', 			    views.InternshipCreateView, name='internship-create'),
-    path('internships/<int:pk>/', 				views.InternshipDetailView, name='internship-detail'),
-    path('internships/<int:pk>/application/', 	views.InternshipApplicationView, name='internship-application'),
-    path('internships/<int:pk>/update/', 		views.InternshipUpdateView.as_view(), name='internship-update'),
-    path('internships/<int:pk>/delete/', 		views.InternshipDeleteView.as_view(), name='internship-delete'),
-    path('venture_capitalist/', 				views.VenCapitalist, name='venture-capitalist'), 
-    path('create_vencap/', 						views.VenCapCreateView,),
-    path('venture_capitalist/<int:pk>/update/', views.VenCapUpdateView.as_view(), name='vencap-update'),
-    path('venture_capitalist/<int:pk>/delete/', views.VenCapDeleteView.as_view(), name='vencap-delete'),
-=======
+
     path('internships/', 						views.Internships,                      name='internships'),
     path('internship/create/', 					views.InternshipCreateView,),
     path('internships/<int:pk>/', 				views.InternshipDetailView,             name='internship-detail'),
@@ -25,6 +14,6 @@ urlpatterns = [
     path('venture_capitalist/create/', 			views.VenCapCreateView,                 name='vencap-create'),
     path('venture_capitalist/<int:pk>/update/', views.VenCapUpdateView,                 name='vencap-update'),
     path('venture_capitalist/<int:pk>/delete/', views.VenCapDeleteView.as_view(),       name='vencap-delete'),
->>>>>>> d080964b2372a4881fe31877d6912220a70c6ed0
+
     
 ]
