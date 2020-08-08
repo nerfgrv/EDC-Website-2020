bf1829a49e881502def91ccfec05298bf9895cfc
Kajal-11 <kajal11122000@gmail.com>
2020-08-08 15:33:36 +0530

    tiemr removed

--- user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, StudentProfile, StartupProfile
-
-admin.site.register(User)
-admin.site.register(StudentProfile)
-admin.site.register(StartupProfile)
